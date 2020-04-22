dc422becb2b51e9719def7bf1169c7ae1efbdf13
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-22 20:37:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,17 @@
 def find_min_in_nested_arrays(src)
   array_of_daily_low_temps = []
   row_index = 0
-  daily_low = ""
+  
   while row_index < src.count do
     element_index = 0
+    daily_low = ""
   while element_index < src[row_index].count do
     if src[row_index][element_index] < daily_low
-      array_of_daily_low_temps << src[row_index][element_index]
+      daily_low = src[row_index][element_index]
   end   
-  element_index += 0
-  end
+  element_index += 1
 end
 
-array_of_daily_low_temps
+array_of_daily_low_temps << daily_low
+row_index += 1
+end
